10a4bb7b71eafab995ea730fa5064400e51b1a0b
lawalgodwin <godwinl200@gmail.com>
2023-11-04 12:18:26 +0100

    Implement a Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -47,6 +47,5 @@ class RedactingFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
         """obfuscate and format record before logging"""
         message = filter_datum(self.fields, self.REDACTION,
-                            super(RedactingFormatter, self).format(record),
-                            self.SEPARATOR)
+                            super().format(record), self.SEPARATOR)
         return message
