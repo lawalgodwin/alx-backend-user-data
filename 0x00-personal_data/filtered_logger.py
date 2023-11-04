bba033a78e2c62396172cb28c4a397f1d80d2763
lawalgodwin <godwinl200@gmail.com>
2023-11-04 10:55:40 +0100

    Obfuscate sensitive data in a log

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Using Regex: Write a function called filter_datum that returns
+the log message obfuscated:
+
+Arguments:
+fields: a list of strings representing all fields to obfuscate
+
+redaction: a string representing by what the field will be obfuscated
+
+message: a string representing the log line
+
+separator: a string representing by which character is separating all
+fields in the log line (message) The function should use a regex to
+replace occurrences of certain field values.
+
+filter_datum should be less than 5 lines long and use re.sub to
+perform the substitution with a single regex
+"""
+
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """Obfucate data by the given field"""
+    for field in fields:
+        message = re.sub(fr'{field}=(?:.+?){separator}',
+                         f'{field}={redaction}{separator}',
+                         message)
+    return message
