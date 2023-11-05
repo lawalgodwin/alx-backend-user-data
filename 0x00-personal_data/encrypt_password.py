3e9848f68397d6d4d8fbd3d72d2400b020c8b150
lawalgodwin <godwinl200@gmail.com>
2023-11-05 18:23:50 +0100

    Salt and hash user password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""User passwords should NEVER be stored in plain text in a database.
+
+Implement a hash_password function that expects one string
+argument name password and returns a salted, hashed password,
+which is a byte string.
+
+Use the bcrypt package to perform the hashing (with hashpw)
+"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Return a byte string of a salted hashed password"""
+    password_in_bytes_form = bytes(password, encoding='UTF-8')
+    return bcrypt.hashpw(password_in_bytes_form, bcrypt.gensalt())
