90c0130fd45fdec9e28e36865f9a7ee36beac076
lawalgodwin <godwinl200@gmail.com>
2023-11-05 16:59:21 +0100

    Get mysql database connector object

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -70,9 +70,7 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Returns a connector to the mysql database
-    NB: pip3 install mysql-connector-python
-    """
+    """Returns a connector to the mysql database"""
     DB_HOST = getenv("PERSONAL_DATA_DB_HOST", "localhost")
     DB_NAME = getenv("PERSONAL_DATA_DB_NAME")
     DB_USER = getenv("PERSONAL_DATA_DB_USERNAME", "root")
