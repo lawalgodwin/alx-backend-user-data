f4b5e8443667a10216509fd4f533bef6f6748ee9
lawalgodwin <godwinl200@gmail.com>
2023-11-07 15:21:31 +0100

    Add forbidden error handler to the app

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,8 +16,14 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """Unauthorized handler"""
+    """User not authenticated handler"""
     return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """User unthenticated but not authorised Handler"""
+    return jsonify({"error": "Forbidden"}), 403
 
 
 @app.errorhandler(404)
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauthorized() -> None:
     """Raise 401 error"""
     return abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> None:
+    """Raise 403 error"""
+    return abort(403)
