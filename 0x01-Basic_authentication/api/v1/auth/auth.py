6e746049d73664dbb04b6ba557569d1bfb195c8f
lawalgodwin <godwinl200@gmail.com>
2023-11-07 15:56:33 +0100

    Remove blank lines containing whitespace in auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,11 +13,11 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Docs strings"""
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """request will be the Flask request object"""
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """request will be the Flask request object"""
         return None
