3449758495a8c9b74e196138d71193de03d0b1c5
lawalgodwin <godwinl200@gmail.com>
2023-11-09 17:55:05 +0100

    White-list the /login path

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -36,13 +36,11 @@ def validate_request():
         return
     isAuthRequired = auth.require_auth(request.path, excluded_paths)
     if isAuthRequired:
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
         if auth.authorization_header(request) is None \
            and auth.session_cookie(request) is None:
             abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
     request.current_user = auth.current_user(request)
 
