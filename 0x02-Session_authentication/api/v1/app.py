39209d91ae63dca3328bf7fd56f5d8d6d80c62a4
lawalgodwin <godwinl200@gmail.com>
2023-11-09 16:03:36 +0100

    Add an empty class that manages our session based authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -17,6 +17,9 @@ AUTH_TYPE = os.getenv('AUTH_TYPE')
 if AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+if AUTH_TYPE == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 else:
     from api.v1.auth.auth import Auth
     auth = Auth()
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A module containing session authentication manager class"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Session manager class"""
+    pass
