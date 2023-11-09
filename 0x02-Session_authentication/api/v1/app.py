aac6577a58ea00e7cee8c521a2ffbb78879379cd
lawalgodwin <godwinl200@gmail.com>
2023-11-09 17:40:09 +0100

    White-list the /login path

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -40,8 +40,8 @@ def validate_request():
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
-        if (auth.authorization_header(request) is None
-            and auth.session_cookie(request) is None):
+        if auth.authorization_header(request) is None \
+           and auth.session_cookie(request) is None:
             abort(401)
 
     request.current_user = auth.current_user(request)
