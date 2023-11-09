f440aa9591db9d6421cf87328ac0125506773012
lawalgodwin <godwinl200@gmail.com>
2023-11-09 16:22:38 +0100

    Create session store for storing users session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,22 @@
 """A module containing session authentication manager class"""
 
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Session manager class"""
-    pass
+    user_id_by_session_id = {}
+
+    def __init__(self) -> None:
+        """class object initializer"""
+        super().__init__()
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a Session ID for a user_id
+        user_id can now be retreived nased on the Session ID"""
+        if user_id is None or (not isinstance(user_id, str)):
+            return None
+        session_id = str(uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
