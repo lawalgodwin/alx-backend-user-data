9766c1772dd962cab91058e4ee60ad75082fd523
lawalgodwin <godwinl200@gmail.com>
2023-11-10 11:09:57 +0100

    Handle logout request

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -37,3 +37,16 @@ class SessionAuth(Auth):
         user_id = self.user_id_for_session_id(session_id)
         user = User.get(user_id)
         return user
+
+    def destroy_session(self, request=None):
+        """ Deletes the user session when /logout is visit"""
+        if request is None:
+            return False
+        session_id= self.session_cookie(request)
+        if not session_id:
+            False
+        user_id = self.user_id_for_session_id(session_id)
+        if not user_id:
+            False
+        del self.user_id_by_session_id[session_id]
+        return True
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -2,7 +2,7 @@
 """A module containing login handler(view)"""
 
 from api.v1.views import app_views
-from flask import request, jsonify, session
+from flask import request, jsonify, abort
 from models.user import User
 import os
 
@@ -34,3 +34,13 @@ def login():
     response = jsonify(user.to_json())
     response.set_cookie(session_cookie, session_id)
     return response
+
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
+def logout():
+    """Handle session auth logout request"""
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)
