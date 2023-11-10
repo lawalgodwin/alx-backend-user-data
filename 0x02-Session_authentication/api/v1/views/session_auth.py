9877f6e817ffd490790f9b90b39dedcde1b321ff
lawalgodwin <godwinl200@gmail.com>
2023-11-10 10:05:22 +0100

    Handle session authentication login request

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -13,22 +13,22 @@ def login():
     email = request.form.get('email')
     password = request.form.get('password')
     if email is None or email == '':
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
     if password is None or password == '':
-        return jsonify({ "error": "password missing" }), 400
+        return jsonify({"error": "password missing"}), 400
     try:
         users = User.search({"email": email})
     except Exception:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
     if not users:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
     user = users[0]
     if not user.is_valid_password(password):
-        return jsonify({ "error": "wrong password" }), 401
+        return jsonify({"error": "wrong password"}), 401
     from api.v1.app import auth
     # create session_id for the user
     session_cookie = os.getenv('SESSION_NAME')
-    session_id= auth.create_session(user.id)
+    session_id = auth.create_session(user.id)
     # set session cookie on response header
     # then send the user as response body in json string
     response = jsonify(user.to_json())
