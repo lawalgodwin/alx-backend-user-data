50036fc6c54c490eb7cd1d60a5668c9fb811f18a
lawalgodwin <godwinl200@gmail.com>
2023-11-14 16:46:02 +0100

    Encrypt password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""A module containing password encryptionn functionality"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Encrypt the given plain text password"""
+    plain_password = password.encode('utf-8')
+    encrypted_password= bcrypt.hashpw(plain_password, bcrypt.gensalt())
+    return encrypted_password
