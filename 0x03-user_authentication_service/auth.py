66cd6f4d59106322cb31ab7516e63b6d90446dec
lawalgodwin <godwinl200@gmail.com>
2023-11-14 17:45:02 +0100

    Register user if not exists

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """A module containing password encryptionn functionality"""
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -8,3 +11,24 @@ def _hash_password(password: str) -> bytes:
     plain_password = password.encode('utf-8')
     encrypted_password = bcrypt.hashpw(plain_password, bcrypt.gensalt())
     return encrypted_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """Class initializer"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user with the specified email and password"""
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            password_hash = _hash_password(password)
+
+            new_user: User = self._db.add_user(email, password_hash)
+            return new_user
+        else:
+            raise ValueError('User <{}> already exists'.format(email))
