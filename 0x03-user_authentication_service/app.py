30e830e58447ca857ccba4df723fdbd39a3438ab
lawalgodwin <godwinl200@gmail.com>
2023-11-14 20:46:51 +0100

    Add login functionality to the app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -43,7 +43,7 @@ def login():
     session_id = AUTH.create_session(email)
     response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie('session_id', session_id)
-    return response        
+    return response
 
 
 if __name__ == "__main__":
