9c24f31b408f8b55ea6f31a603af93dc92d251d5
lawalgodwin <godwinl200@gmail.com>
2023-11-14 16:47:09 +0100

    Encrypt password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,5 +6,5 @@ import bcrypt
 def _hash_password(password: str) -> bytes:
     """Encrypt the given plain text password"""
     plain_password = password.encode('utf-8')
-    encrypted_password= bcrypt.hashpw(plain_password, bcrypt.gensalt())
+    encrypted_password = bcrypt.hashpw(plain_password, bcrypt.gensalt())
     return encrypted_password
