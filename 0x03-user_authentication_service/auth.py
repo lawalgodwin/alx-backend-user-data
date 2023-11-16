c970e5b645da08cc8d87ce3a903003584743cd62
lawalgodwin <godwinl200@gmail.com>
2023-11-16 10:23:53 +0100

    Get user based on its session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 from uuid import uuid4
+from typing import Optional
 
 
 def _hash_password(password: str) -> bytes:
@@ -57,3 +58,10 @@ class Auth:
         user.session_id = _generate_uuid()
         self._db.update_user(user.id)
         return user.session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Optional[User]:
+        """Get user based on its session id"""
+        if session_id is None:
+            return None
+        user = self._db.find_user_by(session_id=session_id)
+        return user if user else None
