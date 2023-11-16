5316c6dd30d200aff66e74364050dbebe7262cde
lawalgodwin <godwinl200@gmail.com>
2023-11-16 10:40:06 +0100

    Destroy a user session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -68,3 +68,14 @@ class Auth:
         except NoResultFound:
             return None
         return user
+
+    def destroy_session(self, user_id: str) -> None:
+        """Destroy session for the specified user"""
+        if user_id is None:
+            return None
+        try:
+            self._db.find_user_by(id=user_id)
+        except NoResultFound:
+            return None
+        self._db.update_user(user_id, session_id=None)
+        return None
