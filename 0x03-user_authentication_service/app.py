501e65a33eeeb0ccb87985f05c094d53853f4a3f
lawalgodwin <godwinl200@gmail.com>
2023-11-16 12:02:18 +0100

    Implement reset password functionality on the app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -67,5 +67,19 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """A function that handles password reset"""
+    email = request.form.get('email')
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({
+        "email": "<{}>".format(email),
+        "reset_token": "<{}>".format(reset_token)
+        }), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
