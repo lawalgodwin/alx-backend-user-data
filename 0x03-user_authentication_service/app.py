bc72fea88175be32cb10309632981328960aeb47
lawalgodwin <godwinl200@gmail.com>
2023-11-16 11:21:17 +0100

    Get User profile from the session ID

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -57,5 +57,15 @@ def logout() -> None:
     return redirect(url_for('home'))
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """Return the profile of a user based on its session ID"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
